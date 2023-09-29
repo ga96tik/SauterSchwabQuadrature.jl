ad178cf778332a423e6f989e03aee06ec1ee1e54
Kristof Cools <krcools@gmail.com>
2023-09-29 17:40:43 +0200

    format make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,14 +6,12 @@ makedocs(;
     modules=[SauterSchwabQuadrature],
     sitename="SauterSchwabQuadrature.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true", canonical="https://ga96tik.github.io/SauterSchwabQuadrature.jl", edit_link="master", assets=String[]
+        prettyurls=get(ENV, "CI", "false") == "true",
+        canonical="https://ga96tik.github.io/SauterSchwabQuadrature.jl",
+        edit_link="master",
+        assets=String[],
     ),
-    pages=[
-        "Introduction" => "index.md",
-        "Details" => "details.md",
-        "Manual" => "manual.md",
-        "API Reference" => "apiref.md",
-    ],
+    pages=["Introduction" => "index.md", "Details" => "details.md", "Manual" => "manual.md", "API Reference" => "apiref.md"],
 )
 
 deploydocs(; repo="github.com/ga96tik/SauterSchwabQuadrature.jl.git", devbranch="master")
